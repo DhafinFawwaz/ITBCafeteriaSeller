e63c952cfde4c4d37afc29e400b1c3bd641ef177
spacetimist <yasrazhaf@gmail.com>
2023-08-07 08:42:22 +0700

    History and Profile Update

--- lib/screens/history/history.dart
+++ lib/screens/history/history.dart
@@ -1,4 +1,8 @@
+import 'package:itb_cafeteria_seller/utils/GlobalTheme.dart';
+import 'package:itb_cafeteria_seller/widgets/ink_well_button.dart';
 import 'package:flutter/material.dart';
+import 'package:itb_cafeteria_seller/const.dart';
+import '../../widgets/custom_menu_base.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -8,10 +12,116 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
+        Widget buildHistory() {
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: GlobalTheme.padding1, vertical: 10),
+            child: InkWellButton(
+              onPressed: () {},
+              child: Padding(
+                padding: EdgeInsets.all(18),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Maulvi", style: TextStyle(fontWeight: FontWeight.w800),),
+                        SizedBox(height: 3),
+                        Text("1 Kopi, 1 Nasi ayam"),
+                        SizedBox(height: 3),
+                        Text("Rp45.000", style: TextStyle(fontWeight: FontWeight.w600)),
+                        SizedBox(height: 3),
+                        Text("29 Feb 09:23 AM"),
+                      ],
+                    ),
+                    Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text("Selesai", style: TextStyle(fontWeight: FontWeight.w800),),
+                        SizedBox(height: 3),
+                        Text("Pesanan A-120412"),
+                        SizedBox(height: 6),
+                        Text("29 Feb 09:23 AM"),
+                      ],
+                    ),
+                  ]
+                ),
+              ),
+            )
+          );
+        }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("History"),
+    return CustomMenuBase(
+      hoveringChild: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('History', style: TextStyle(
+            color: Colors.black,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+              ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              InkWellButton(
+                  onPressed: () {},
+                  child: Padding( padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+                    child: Row( children: <Widget>[
+                      Text(
+                        "Tanggal",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.black,
+                      ),
+                    ]))),
+
+              SizedBox(width: 10.0),
+
+              InkWellButton(
+                  onPressed: () {},
+                  child: Padding( padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+                    child: Row( children: <Widget>[
+                      Text(
+                        "Metode",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.black,
+                      ),
+                    ]))),
+
+            ],
+          ),
+        ],
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+            buildHistory(),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../widgets/custom_menu_base.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -10,256 +11,256 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('My Store', style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        )),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Divider(
-              color: Colors.grey[800],
-              height: 30.0,
-            ),
-            Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 40.0,
-                  ),
-                  SizedBox(width: 20.0),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Nama toko',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                          ),
+    return CustomMenuBase(
+      hoveringChild: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('My Store', style: TextStyle(
+            color: Colors.black,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          Divider(
+            color: Colors.grey[800],
+            height: 30.0,
+          ),
+          Row(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Nama toko',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
                         ),
-                        Text(
-                          'alamat email/no telp',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 15.0,
-                          ),
+                      ),
+                      Text(
+                        'alamat email/no telp',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15.0,
+                        ),
+                      )
+                    ]),
+                SizedBox(width: 20.0),
+                FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.grey[200],
+                    elevation: 0.0,
+                    child: Text(
+                        'Edit',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black
                         )
-                      ]),
-                  SizedBox(width: 20.0),
-                  FloatingActionButton(
-                      onPressed: () {},
-                      backgroundColor: Colors.grey[200],
-                      elevation: 0.0,
-                      child: Text(
-                          'Edit',
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.black
-                          )
-                      )
-                  )
-                ]
+                    )
+                )
+              ]
+          ),
+          Divider(
+            color: Colors.grey[800],
+            height: 30.0,
+          ),
+          SizedBox(height: 10.0),
+          Text(
+            'Pengaturan Akun',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
-            Divider(
-              color: Colors.grey[800],
-              height: 30.0,
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Pengaturan Akun',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.money,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Pembayaran',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'Ewallet, kredit',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.location_city,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Lokasi Kantin',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'GKUB',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.security,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Keamanan',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'Login 2FA, password, biometrik',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.privacy_tip,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Privasi',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'S&K',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.notification_add,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Notifikasi',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'Muted',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.logout,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 10.0),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Logout',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.5,
-                        ),
-                      ),
-                      Text(
-                        'keluar dari akun',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 15.0,
-                        ),
-                      )
-                    ]),
-              ],
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20.0),
+        ],
       ),
-
+      child: Padding(
+      padding: const EdgeInsets.fromLTRB(30.0, 150.0, 30.0, 0),
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.money,
+                color: Colors.black,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Pembayaran',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'Ewallet, kredit',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.location_city,
+                color: Colors.black,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Lokasi Kantin',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'GKUB',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.security,
+                color: Colors.black,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Keamanan',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'Login 2FA, password, biometrik',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.privacy_tip,
+                color: Colors.black,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Privasi',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'S&K',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.notification_add,
+                color: Colors.black,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Notifikasi',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'Muted',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.logout,
+                color: Colors.red,
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.5,
+                      ),
+                    ),
+                    Text(
+                      'keluar dari akun',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+            ],
+          ),
+          Spacer(),
+        ],
+      ),
+    )
     );
   }
 }
